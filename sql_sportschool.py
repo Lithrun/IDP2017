1271b4d51b6aa2b6e10f7b15e2bea67d66361287
Nickernator <nickwindt@hotmail.nl>
2017-01-21 23:16:35 +0100

    Update sql_sportschool.py

--- sql_sportschool.py
+++ sql_sportschool.py
@@ -1,12 +1,62 @@
 # MySQL settings
 username = 'benno'
-name = ''
+global databasename
+databasename = 'sportschool'
 password = ''
-port = ''
+host = ''
 # Einde MySQL settings
+
+import sqlite3
+#import mysql.connector
+
+
 
 def createTables():
     pass
 
-def connectToDatabase():
-    pass
+#def connectToDatabase(): # My SQL
+#    cnx = mysql.connector.connect(user=username, password=password,
+#                              host=host,
+#                              database=databasename)
+#    cnx.close()
+
+def isDatabaseConnected(databasename):
+    try:
+        connect = sqlite3.connect(databasename)
+        return True
+    except:
+        return False
+
+def startDatabase(databasename): # Maak tabellen aan als deze nog niet bestaan
+    if isDatabaseConnected(databasename) == True:
+        connect = sqlite3.connect(databasename)
+        c = connect.cursor()
+        # Create table
+        c.execute('''CREATE TABLE IF NOT EXISTS accounts
+                         (gebruikersnaam text UNIQUE, email text, wachtwoord text, laatste_login_datum text)''')
+
+        gegevens = ['gebruiker','gebruiker@hotmail.nl','5375','11-11-2016']
+        try:
+            c.execute('''INSERT INTO accounts VALUES(?,?,?,?)''',gegevens)
+        except:
+            print('Account bestaat al')
+        finally:
+            connect.commit()
+
+startDatabase(databasename)
+
+def isLoginCorrect(gebruikersnaam,wachtwoord):
+    import sqlite3
+    database = databasename
+    if isDatabaseConnected(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        gegevens = [gebruikersnaam, wachtwoord]
+        c.execute('''SELECT * FROM accounts WHERE gebruikersnaam = ? AND wachtwoord = ?''',gegevens)
+        resultaten = c.fetchall()
+        # controleer of gebruikersnaam EN wachtwoord overeenkomen met de opgegeven data
+        for resultaat in resultaten:
+            if resultaat[0] == gebruikersnaam and resultaat[2] == wachtwoord:
+                connect.close()
+                return True
+        return False
