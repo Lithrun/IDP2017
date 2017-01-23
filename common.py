4733a53df5e4d90881b7a97640f07ad2afe26948
Nickernator <nickwindt@hotmail.nl>
2017-01-23 23:12:23 +0100

    Update common.py

--- common.py
+++ common.py
@@ -4,6 +4,12 @@ def getCurrentDate():
     datum = nu.strftime('%d-%m-%Y')
     return datum
 
+def getListSize(list):
+    var = len(list)
+    if var:
+        return var
+    return False
+
 def isInt(integer):
     try:
         integer = int(integer)
